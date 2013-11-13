9c20a7cb791271670c31de4931afa968ebae35d8
Tobias Kahan <tobyk100@gmail.com>
2013-11-13 15:53:55 -0800

    Update Android installer.

--- home/.vm.sh
+++ home/.vm.sh
@@ -7,7 +7,7 @@ mkdir -p ~/dev/vm
   curl -O "https://az412801.vo.msecnd.net/vhd/IEKitV1_Final/VMWare_Fusion/IE8_XP/IE8.XP.For.MacVMware.sfx"
 )
 
-android update sdk --no-ui
+android update sdk --no-ui --all
 (
   cd ~/dev
   git clone https://github.com/j5at/AndroidAVDRepo.git
