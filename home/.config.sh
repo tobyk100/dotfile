e6d76195c9a2b35fc972a1c8d14323674f750098
Toby <tobyk100@gmail.com>
2014-10-06 18:08:09 -0700

    Moved setup to other repo, got rid of vm stuff.

--- home/.config.sh
+++
@@ -1,7 +0,0 @@
-gem install homesick
-homesick clone https://github.com/tobyk100/dotfiles.git
-homesick symlink dotfiles
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-source ./.brew
-
-npm install -g jshint
